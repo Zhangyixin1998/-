89ca3f72273df48bfdd98748428cc9d9fe227e12
Zhangyixin1998 <972259655@qq.com>
2020-03-08 12:14:13 +0800

    Add files via upload

---
+++ lecture2_homework/pages/resume.js
@@ -0,0 +1,99 @@
+// pages/resume/resume.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    isAdd:false,
+    eduList:[
+      {
+        src:"../../images/primary.png",
+        info:"2004.9-2010.6 无锡市通江实验小学"
+      },
+      {
+        src: "../../images/middle.png",
+        info: "2010.9-2013.6 无锡市大桥实验中学"
+      },
+      {
+        src: "../../images/high.png",
+        info: "2013.9-2016.6 无锡市第一中学"
+      },
+      {
+        src: "../../images/university.png",
+        info: "2016.9-2020.6 北京语言大学"
+      }
+    ],
+    praiseList:[
+      {
+        id:"01",
+        praise:"三次学年奖学金"
+        },
+      {
+        id:"02",
+        praise:"三好学生及优秀学生干部"
+        },
+      {
+        id:"03",
+        praise:"catti三级笔译"
+        }
+    ]
+  },
+  
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
